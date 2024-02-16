a15a6b8af42a5751a5ff77a1d47ff7fb0c5bb3fa
Дарья Коннова <dashustic@yandex.ru>
2024-02-16 22:12:59 +0300

    redis cache

--- app/api/endpoints/currency.py
+++ app/api/endpoints/currency.py
@@ -1,38 +1,46 @@
 from datetime import datetime
 
 import aiohttp
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException, status
 
-from app.api.models import User
+
 from app.api.schemas import ResponseCurrency
 from app.core.config import settings
-from app.utils.users import get_current_user
+
+from redis import asyncio as aioredis
 
 router = APIRouter(prefix="/currency", tags=["Currency"])
 
+redis = aioredis.from_url(
+    "redis://localhost:6379", encoding="utf8", decode_responses=True
+)
+
 
 @router.get("/exchange_rate")
-async def get_exchange_rates(
-        source: str = 'USD', currencies: str = None
-):
-    a = currencies.split(',')
-    curren = '%2C'.join(a) if len(a) > 1 else a[0]
-    url = f"{settings.API.EXCRATES}?source={source}&currencies={currencies}"
+async def get_exchange_rates(source: str = "USD", currencies: str = None):
+    cache = await redis.get("currencies")
+
+    currency_list = currencies.split(",")
+    if not all(elem in cache for elem in currency_list):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect currency code!"
+        )
+
+    param_currency = (
+        "%2C".join(currency_list) if len(currency_list) > 1 else currency_list[0]
+    )
+    url = f"{settings.API.EXCRATES}?source={source}&currencies={param_currency}"
     headers = {"apikey": settings.API.KEY}
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
             if response.status == 200:
                 data = await response.json()
                 response = ResponseCurrency(
-                    time=datetime.utcfromtimestamp(data['timestamp']).strftime('%Y-%m-%d %H:%M:%S'),
-                    source=data['source'],
-                    quotes=data['quotes']
-                    )
+                    time=datetime.utcfromtimestamp(data["timestamp"]).strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    ),
+                    source=data["source"],
+                    quotes=data["quotes"],
+                )
 
     return response
-
-
-
-
-# {'success': True, 'timestamp': 1707944163, 'source': 'USD', 'quotes': {'USDEUR': 0.93219, 'USDRUB': 91.650152}}
-# datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
